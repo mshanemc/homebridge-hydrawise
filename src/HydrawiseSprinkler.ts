6dc90862becb7a72cbc88e484b81171ea93cdf84
mshanemc <shane.mclaughlin@salesforce.com>
2020-07-17 18:47:20 -0500

    reactivate the "on" option but keep it 2nd

--- src/HydrawiseSprinkler.ts
+++ src/HydrawiseSprinkler.ts
@@ -136,16 +136,16 @@ export class HydrawiseSprinkler {
           // Run zone
           if (value == 1) {
             callback();
-            // that.zone
-            //   .run()
-            //   .then((data) => {
-            //     that.platform.log.info(that.zone.name + ' sprinkler turned on');
-            //     callback();
-            //   })
-            //   .catch((error) => {
-            //     that.platform.log.error(error);
-            //     callback();
-            //   });
+            that.zone
+              .run()
+              .then((data) => {
+                that.platform.log.info(that.zone.name + ' sprinkler turned on');
+                callback();
+              })
+              .catch((error) => {
+                that.platform.log.error(error);
+                callback();
+              });
           }
           // Stop running zone
           else {
