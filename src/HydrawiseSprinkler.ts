2bb67f3f14a8b740cf995821370cdd6c6250295a
mshanemc <shane.mclaughlin@salesforce.com>
2020-07-17 17:39:33 -0500

    is duration in hours?

--- src/HydrawiseSprinkler.ts
+++ src/HydrawiseSprinkler.ts
@@ -147,8 +147,9 @@ export class HydrawiseSprinkler {
       .on(
         CharacteristicEventTypes.SET,
         (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
+          that.platform.debug(`duration requested: ${value}`);
           that.zone
-            .run(value as number)
+            .run((value as number) / 60)
             .then((data) => {
               that.platform.log.info(
                 'Set Duration for ' + zone.name + ' to ' + value
