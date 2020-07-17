a149dfb5bf61bfe9578511aed91662ea35fd8a2c
mshanemc <shane.mclaughlin@salesforce.com>
2020-07-17 16:24:45 -0500

    don't keep dist

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const settings_1 = require("./settings");
-const HydrawisePlatform_1 = require("./HydrawisePlatform");
-module.exports = (api) => {
-    api.registerPlatform(settings_1.PLUGIN_NAME, settings_1.PLATFORM_NAME, HydrawisePlatform_1.HydrawisePlatform);
-};
-//# sourceMappingURL=index.js.map
